e34677c245a75855faec52ced51d2bf7eade75ce
Aliciazyn <142967610+AiciaxyviorMd@users.noreply.github.com>
2023-10-23 16:39:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 import { join, dirname } from 'path';
-import { fileURLToPath } 'uril';
-import { spawn } from 'child_process')
-import { watchfile, unwatchFile } from 'fs',
+import { fileURLToPath } from 'url';
+import { spawn } from 'child_process';
+import { watchFile, unwatchFile } from 'fs';
 
-const __dirname = diname(fileURLToPath(import.meta.url))
-console.info([System] Starting...')
+const __dirname = dirname(fileURLToPath(import.meta.url));
+console.info('[System] Starting...');
 
 function start() {
- let args = [join(__dirname,'main.js'), ...process.argv.slice(2)];
- let p = spawn(process.argv[0], args, ( studio: ['inherit', 'inherit', 'inherit', 'ipc'] });
- .on('message',data => {
-   if (data == 'reset') {
-    console.info(['System] Restarting...');
-    p.kill()
-    start()
-   };
+  let args = [join(__dirname, 'main.js'), ...process.argv.slice(2)];
+  let p = spawn(process.argv[0], args, { stdio: ['inherit', 'inherit', 'inherit', 'ipc'] });
+
+  p.on('message', (data) => {
+    if (data === 'reset') {
+      console.info('[System] Restarting...');
+      p.kill();
+      start();
+    }
   });
-  .on('exit'. (_, code) => {
-    if(code !== o) start()
+
+  p.on('exit', (code) => {
+    if (code !== 0) start();
     watchFile(args[0], () => {
-      unwatchFile(args[0])
+      unwatchFile(args[0]);
       start();
     });
-   });
-  };    
-  start();
+  });
+}
+
+start();
